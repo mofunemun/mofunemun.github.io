6fc426546254fb6719911a22ff648501f3372aed
mofunemun <sinkamofunemu@gmail.com>
2022-02-14 12:11:54 +0900

    search done

---
+++ app.js
@@ -0,0 +1,65 @@
+'use strict'
+
+async function getShop(){
+    // 位置情報取得
+    navigator.geolocation.getCurrentPosition(Success,Fail);
+    async function Success(pos){
+        let lat = pos.coords.latitude;
+        let lng = pos.coords.longitude;
+
+        // 検索範囲を指定したときの処理
+        document.getElementById('range').onsubmit = async function(event){
+            // アクションによる再読み込みを防止する
+            event.preventDefault();
+
+            // 前回の検索結果を消去
+            let lastItems = document.getElementById("answers");
+            lastItems.remove();
+
+            // 検索範囲と位置情報をURLに組み込んで呼び出し
+            const range = document.getElementById("range").rangeNum.value;
+            let url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=5231d4b646e96da3&lat=" + lat + "&lng=" + lng + "&range=" + range + "&format=json";
+            const res = await fetch(url);
+            const users = await res.json();
+            console.log(users);
+
+            // 検索結果がなければメッセージを、あればそれをすべて出す
+            document.getElementById("app").insertAdjacentHTML("beforeend", "<div id='answers'></div>");
+            let shopNum = users.results.shop.length;
+            if(shopNum === 0){
+                document.getElementById("answers").insertAdjacentHTML("beforeend", "<p>大変申し訳ありません。検索した範囲内に店舗がありません。</p>");
+            }else{
+                for (let i=0; i < shopNum; i++){
+                    let itemName = users.results.shop[i].name;
+                    let itemPlace = users.results.shop[i].address;
+                    let itemStation = users.results.shop[i].station_name;
+                    let itemImg = users.results.shop[i].photo.pc.m;
+
+                    let shopBox = "<div id='" + i + "'></div>"
+                    let shopH2 = "<h2>" + itemName + "</h2>";
+                    let shopPlace = "<p>" + itemPlace + "</p>";
+                    let shopStation = "<p>" + itemStation + "</p>";
+                    let shopImg = "<img src='" + itemImg + "' alt='" + itemName + "の写真です'>"
+
+                    document.getElementById("answers").insertAdjacentHTML("beforeend", shopBox);
+                    document.getElementById(i).insertAdjacentHTML("beforeend", shopH2);
+                    document.getElementById(i).insertAdjacentHTML("beforeend", shopPlace);
+                    document.getElementById(i).insertAdjacentHTML("beforeend", shopStation);
+                    document.getElementById(i).insertAdjacentHTML("beforeend", shopImg);
+                }
+            }
+        }
+    }
+    // 位置情報エラー時の対応
+    function Fail(error){
+        if(error === 1){
+            alert("エラー！位置情報を取得する権限がありません");
+        }else if(error === 2){
+            alert("エラー！通信上のエラーです");
+        }else{
+            alert("エラー！タイムアウトしました");
+        }
+    }
+}    
+
+getShop();
